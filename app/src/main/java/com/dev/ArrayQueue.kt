8cf836fa7c45888b0959d409d014f4f3f86a277a
David B ZHANG <18588744489@163.com>
2023-05-19 21:57:55 +0800

    Update

--- app/src/main/java/com/dev/ArrayQueue.kt
+++ app/src/main/java/com/dev/ArrayQueue.kt
@@ -8,11 +8,22 @@ class ArrayQueue(
     private var tail = 0
 
     fun enqueue(data: String?): Boolean {
-        data ?: return false
         if (tail == capacity) {
-            return false
+            if (head == 0) return false // present the queue is full
+
+            // Data migration
+            /** 当队列的 tail 指针移动到数组的最右边后，如果有新的数据入队，我们可以将 head 到 tail 之间的数据，整体搬移到数组中 0 到 tail-head 的位置 */
+            (head until tail).forEach { i ->
+                array[i - head] = array[i]
+            }
+
+            // Update head & tail
+            tail -= head
+            head = 0
         }
-        array[tail++] = data
+
+        array[tail] = data
+        ++tail
         return true
     }
 
