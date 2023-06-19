c99414b28063c518613356cf4c058e85a4423da8
David B ZHANG <18588744489@163.com>
2023-06-19 21:07:30 +0800

    Update

--- app/src/test/java/com/dev/array/day3/Day5Test.kt
+++ app/src/test/java/com/dev/array/day3/Day5Test.kt
@@ -2,16 +2,16 @@ package com.dev.array.day3
 
 import org.junit.Test
 
-/** 查找第一个大于等于给定值的元素 */
-class Day4 {
+/** 查找最后一个小于等于给定值的元素 */
+class Day5Test {
 
     @Test
     fun test() {
         // GIVEN
-        val array = intArrayOf(3, 4, 6, 7, 10)
+        val array = intArrayOf(3, 5, 6, 8, 9, 10)
 
         // WHEN
-        val result = getResult(array, 5)
+        val result = getResult(array, 7)
 
         // THEN
         assert(2 == result)
@@ -22,14 +22,14 @@ class Day4 {
         var high = array.lastIndex
         while (low <= high) {
             val middle = low + ((high - low) shr 1)
-            if (array[middle] >= target) {
-                if (middle == 0 || array[middle - 1] < target) {
+            if (array[middle] > target) {
+                high = middle - 1
+            } else {
+                if (middle == array.lastIndex || array[middle + 1] > target) {
                     return middle
                 } else {
-                    high = middle - 1
+                    low = middle + 1
                 }
-            } else {
-                low = middle + 1
             }
         }
         return -1
